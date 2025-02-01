2bd366f3d74463616fc41dde6eab5facc0409a15
jeromehaas <hello@jeromehaas.ch>
2025-02-01 13:40:26 +0100

    Refactor: remove partner section

--- pages/aktivitaeten.jsx
+++ pages/aktivitaeten.jsx
@@ -13,7 +13,6 @@ const Aktivitaeten = () => {
 		<Page data={{ title: 'Aktivitäten' }}>
 			<Intro data={{ title: 'Aktivitäten', text: 'Ich bleibe auf dem Laufenden und besuche regelmässig Fortbildungen und Anlässe rund um das Thema Kopf- und Kieferschmerzen sowie Schlafapnoe.', image: {	src: '/images/11-daniela-haerle-portrait-brille.jpg',	alt: 'Dr. med. dent. Daniela Haerle am Vierwaldstättersee in Luzern' }, navigation: {	title: 'Themen dieser Seite',	links: [{ text: 'Weiterbildung / Tagungen', href: 'weiterbildungen-tagungen' },		{ text: 'Partner', href: 'partner' }] } }}/>
 			<Education data={{ anchorId: 'weiterbildungen-tagungen' }} />
-			<Partners data={{ anchorId: 'partner' }} />
 			<CTA data={{ title: 'Termin vereinbaren', text: 'Sie finden mich in der Praxis am Dietschiberg an der Lützelmattstrasse 3 in Luzern.', button: { 	text: 'Kontakt', 	href: '/kontakt' } }} />
 		</Page>
 	);
